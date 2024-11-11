c3339867dbc612e7ed497f39eb223a55549a21b0
sergeymelnikov2024 <7129700ms@gmail.com>
2024-11-11 21:11:18 +0300

    correction in task 4

--- src/main/java/lesson_4/Worker.java
+++ src/main/java/lesson_4/Worker.java
@@ -1,5 +1,7 @@
 package lesson_4;
 
+import java.math.BigDecimal;
+
 public class Worker {
     private String surname;
     private String name;
@@ -7,10 +9,10 @@ public class Worker {
     private String position;
     private String email;
     private String phone;
-    private double salary;
+    private BigDecimal salary;
     private int age;
 
-    public Worker(String surname, String name, String patronymic, String position, String email, String phone, double salary, int age) {
+    public Worker(String surname, String name, String patronymic, String position, String email, String phone, BigDecimal salary, int age) {
         this.surname = surname;
         this.name = name;
         this.patronymic = patronymic;
@@ -33,11 +35,11 @@ public class Worker {
     public static void main(String[] args) {
         Worker[] workers = new Worker[5];
 
-        workers[0] = new Worker("Зайцев", "Петр", "Иванович", "Инженер", "petr.ivanov@gmail.com", "+7 (123) 456-7890", 50000.0, 35);
-        workers[1] = new Worker("Смирнова", "Анна", "Юрьевна", "Менеджер", "anna.smirnova@gmail.com", "+7 (987) 654-3210", 60000.0, 30);
-        workers[2] = new Worker("Маршин", "Александр", "Александрович", "Программист", "ivan.sidorov@gmail.com", "+7 (456) 123-7890", 40000.0, 25);
-        workers[3] = new Worker("Петрова", "Мария", "Николаевна", "Бухгалтер", "maria.petrova@mail.ru", "+7 (111) 222-3344", 45000.0, 28);
-        workers[4] = new Worker("Козлов", "Евгений", "Викторович", "Директор", "evgeny.kozlov@mail.ru", "+7 (555) 666-7788", 80000.0, 40);
+        workers[0] = new Worker("Зайцев", "Петр", "Иванович", "Инженер", "petr.ivanov@gmail.com", "+7 (123) 456-7890", new BigDecimal("50000.0"), 35);
+        workers[1] = new Worker("Смирнова", "Анна", "Юрьевна", "Менеджер", "anna.smirnova@gmail.com", "+7 (987) 654-3210", new BigDecimal("60000.0"), 30);
+        workers[2] = new Worker("Маршин", "Александр", "Александрович", "Программист", "ivan.sidorov@gmail.com", "+7 (456) 123-7890", new BigDecimal("40000.0"), 25);
+        workers[3] = new Worker("Петрова", "Мария", "Николаевна", "Бухгалтер", "maria.petrova@mail.ru", "+7 (111) 222-3344", new BigDecimal("45000.0"), 28);
+        workers[4] = new Worker("Козлов", "Евгений", "Викторович", "Директор", "evgeny.kozlov@mail.ru", "+7 (555) 666-7788", new BigDecimal("80000.0"), 40);
 
         for (Worker worker : workers) {
             worker.printInfo();
